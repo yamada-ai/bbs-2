f241d88123992da0c88ec9a929e1333b8bad9d49
yamada-ai <hatsudaaa@gmail.com>
2024-06-04 22:29:26 +0900

    first commit

---
+++ src/main/java/com/example/Bbs2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbs2Application.class, args);
+	}
+
+}
